a158a96bd1f07d9aa0288771c72e6d806a4c5deb
Peeraphat Charoenthai <peeraphat.ch@kkumail.com>
2023-02-09 22:01:40 +0700

    edit table

--- src/api/trip-history/routes/trip-history.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * trip-history router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::trip-history.trip-history');
--- src/api/trip-history/services/trip-history.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * trip-history service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::trip-history.trip-history');
